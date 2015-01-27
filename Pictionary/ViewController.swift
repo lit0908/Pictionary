c53db9b56185d59ffabb0d288b2b1b21034626bb
Tong Li <tongli0809@gmail.com>
2015-01-27 22:25:30 +1100

    Create Search Bar and Table View with their corresponding Delegate Functions / Data Source.

--- Pictionary/ViewController.swift
+++ Pictionary/ViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,45 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        // This is for resigning first responder when click on empty area on the screen
+        self.view.endEditing(true)
+    }
 
 
+    /*
+        Functions for UISearchBarDelegate
+    */
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        println("Text changed to \(searchText)")
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        println("Search button clicked and resign first responder")
+        
+        searchBar.resignFirstResponder()
+    }
+    
+    /*
+        Functions for UITableViewDataSource
+    */
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = "test cell"
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    /*
+        Functions for UITableViewDataDelegate
+    */
 }
 
